4810fcb6cdfbdb2b6738a8d0090e11a6d27c7dc0
Alex Nepsha <alex.n@fundomate.com>
2023-09-12 10:30:06 +0300

    Single gift page implementation

--- src/components/GiftCard/index.tsx
+++ src/components/GiftCard/index.tsx
@@ -4,13 +4,19 @@ interface Props {
   title: string;
   description: string;
   thumbnail: string;
+  slug: string;
 }
-const GiftCard: React.FC<Props> = ({ title = "", thumbnail, description }) => (
-  <div className={styles.giftCard}>
-    <div>{title}</div>
-    <img src={thumbnail} alt={title} />
-    <div>{description}</div>
-  </div>
+const GiftCard: React.FC<Props> = ({ title, thumbnail, description, slug }) => (
+  <a href={`/gifts/${slug}`} className={styles.giftCard}>
+    <div className={styles.giftCard__imgWrap}>
+      <img className={styles.giftCard__img} src={thumbnail} alt={title} />
+    </div>
+
+    <div>
+      <h1 className={styles.giftCard__heading}>{title}</h1>
+      <p className={styles.giftCard__desc}>{description}</p>
+    </div>
+  </a>
 );
 
 export default GiftCard;
