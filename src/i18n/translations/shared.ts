5ac4e4ef1c9be5c6bfd6bdd83b1984f899ef9b0c
Alex Nepsha <alex.n@fundomate.com>
2023-10-14 13:52:12 +0300

    i18n. Title translations

--- src/i18n/translations/shared.ts
+++ src/i18n/translations/shared.ts
@@ -1,11 +1,13 @@
 import { languages } from "@i18n/ui.ts";
-export type SharedStrings = "s.category" | "s.age";
+export type SharedStrings = "s.category" | "s.age" | "s.gift_idea";
 const shared: Record<keyof typeof languages, Record<SharedStrings, string>> = {
   en: {
+    "s.gift_idea": "gift idea",
     "s.category": "category",
     "s.age": "age",
   },
   ru: {
+    "s.gift_idea": "идея подарка",
     "s.category": "категория",
     "s.age": "возраст",
   },
