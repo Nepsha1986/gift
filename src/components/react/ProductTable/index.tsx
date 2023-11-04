b570a2757ae8632e7724dbdca9378c26c615e130
Alex Nepsha <alex.n@fundomate.com>
2023-11-04 12:19:00 +0300

    Tiny changes

--- src/components/react/ProductTable/index.tsx
+++ src/components/react/ProductTable/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const ProductTable: React.FC<Props> = ({ ideaName }) => {
-  const [idea, setIdea] = useState<IdeaDto>({} as IdeaDto);
+  const [idea, setIdea] = useState<IdeaDto>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
@@ -30,36 +30,39 @@ const ProductTable: React.FC<Props> = ({ ideaName }) => {
 
   return (
     <div>
-      <h2>Happy gift-hunting!</h2>
-      <p>
-        {
-          "Within the list, you'll find a handpicked assortment of gift ideas, each complete with a name, description, and product links. This provides you with a convenient way to browse and discover the ideal gift for any occasion. Enjoy your gift-hunting experience!"
-        }
-      </p>
-
+      {/* @ts-expect-error resolves es-lint conflict */}
       {Object.keys(idea).includes("products") ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Link</th>
-            </tr>
-          </thead>
-          <tbody>
-            {idea.products.map((item, index) => (
-              <tr key={item.title}>
-                <td>{item.title}</td>
-                <td>{item.description}</td>
-                <td>{item.link}</td>
+        <div>
+          <h2>Happy gift-hunting!</h2>
+          <p>
+            {
+              "Within the list, you'll find a handpicked assortment of gift ideas, each complete with a name, description, and product links. This provides you with a convenient way to browse and discover the ideal gift for any occasion. Enjoy your gift-hunting experience!"
+            }
+          </p>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Link</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {idea.products.map((item, index) => (
+                <tr key={item.title}>
+                  <td>{item.title}</td>
+                  <td>{item.description}</td>
+                  <td>{item.link}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       ) : (
         <p>
-          Sorry, Currently there are no products in the list, we are working on
-          this feature....
+          We are sorry, but currently there are no products in the list. We are
+          working on adding this feature, and it will be available soon.
         </p>
       )}
     </div>
