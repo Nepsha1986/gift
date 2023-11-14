dd5b452508b68ac7ac719c3a770f5c0ba37c42c6
Alex Nepsha <nepsha1986@gmail.com>
2023-11-14 15:44:39 +0300

    hotfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://gift-idea.co",
-  integrations: [react(), sitemap()],
-  redirects: {
-    "/admin/[...slug]": "/admin",
-  },
+  integrations: [react(), sitemap()]
 });
