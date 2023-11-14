203c9c2992394a7eabac1b7e8079b305f4f51f84
Alex Nepsha <nepsha1986@gmail.com>
2023-11-14 17:06:52 +0300

    Logged in block (#30)

---
+++ src/features/AdminPanel/layouts/Dashboard/Navigation/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartSimple,
+  faBagShopping,
+  type IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./styles.module.scss";
+
+interface NavItemProps {
+  to: string;
+  label: string;
+  icon: IconDefinition;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ to, label, icon }) => (
+  <li className={styles.navigation__listItem}>
+    <NavLink className={styles.navigation__link} end to={to}>
+      <FontAwesomeIcon icon={icon} />{" "}
+      <span style={{ marginLeft: "10px" }}>{label}</span>
+    </NavLink>
+  </li>
+);
+
+const Navigation: React.FC = () => (
+  <ul className={styles.navigation}>
+    <NavItem to="/admin" icon={faChartSimple} label="Overview" />
+    <NavItem
+      to="/admin/related-products"
+      icon={faBagShopping}
+      label="Products"
+    />
+  </ul>
+);
+
+export default Navigation;
