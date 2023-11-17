7c61e59f0276c83e3b76d5c2a0447d26747fd5da
Alex Nepsha <nepsha1986@gmail.com>
2023-11-17 18:57:27 +0300

    Posts page improvements

--- src/features/AdminPanel/pages/Posts/index.tsx
+++ src/features/AdminPanel/pages/Posts/index.tsx
@@ -1,24 +1,185 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faEye } from "@fortawesome/free-solid-svg-icons";
+
 import { useAstroContext } from "@src/features/AdminPanel/context/astroContext.tsx";
+import Select from "@reactComponents/Select";
+import type { SupportedLocales } from "@i18n/ui.ts";
+import { getCleanSlug, getLocaleFromSlug } from "@i18n/utils.ts";
+import Input from "@src/features/AdminPanel/components/Input";
+import Button from "@reactComponents/Button";
+import type { Category } from "@src/types/category.ts";
+
+const PROJECT_GITHUB_REPO = "https://github.com/Nepsha1986/gift;";
 
 const Posts: React.FC = () => {
   const { ideaPages } = useAstroContext();
+  const [locale, setLocale] = useState<SupportedLocales>();
+  const [category, setCategory] = useState<Category>();
+  const [search, setSearch] = useState<string>();
+
+  const filtered = useMemo(() => {
+    let filteredPages = ideaPages;
+
+    if (locale) {
+      filteredPages = filteredPages?.filter(
+        (i) => getLocaleFromSlug(i.slug) === locale,
+      );
+    }
+
+    if (category) {
+      filteredPages = filteredPages?.filter((i) => i.category === category);
+    }
+
+    if (search) {
+      filteredPages = filteredPages?.filter(
+        (i) =>
+          i.title.toLowerCase().includes(search.toLowerCase()) ||
+          i?.refId?.toLowerCase()?.includes(search.toLowerCase()),
+      );
+    }
+
+    return filteredPages;
+  }, [locale, search, ideaPages, category]);
 
   return (
     <div>
       <h1>Posts</h1>
-      {ideaPages?.map((i, index) => (
-        <div key={i.slug}>
-          {index + 1}.{" "}
-          <a
-            target="_blank"
-            href={`https://github.com/Nepsha1986/gift/edit/main/src/content/gifts/${i.slug}.md`}
-            rel="noreferrer"
-          >
-            {i.title} - {i.slug}
-          </a>
+
+      <div
+        style={{
+          maxWidth: "600px",
+          width: "100%",
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <div style={{ flexGrow: 1 }}>
+          <Input
+            name="searchByName"
+            value={search as string}
+            label="Search by title | refId"
+            onChange={setSearch}
+            placeholder="Search"
+          />
         </div>
-      ))}
+
+        <div style={{ flexGrow: 1 }}>
+          <Select
+            label="Locale"
+            name="locale"
+            value={locale as string}
+            onChange={(val) => {
+              setLocale(val as SupportedLocales);
+            }}
+            options={[
+              {
+                label: "All",
+                value: "",
+              },
+              {
+                label: "English - United States",
+                value: "en-us",
+              },
+              {
+                label: "Ukrainian - Ukraine",
+                value: "uk-ua",
+              },
+              {
+                label: "Russian - Ukraine",
+                value: "ru-ua",
+              },
+            ]}
+          />
+        </div>
+
+        <div style={{ flexGrow: 1 }}>
+          <Select
+            label="Category"
+            name="category"
+            value={category as string}
+            onChange={(val) => {
+              setCategory(val as Category);
+            }}
+            options={[
+              {
+                label: "All",
+                value: "",
+              },
+              {
+                label: "For men",
+                value: "for-men",
+              },
+              {
+                label: "For women",
+                value: "for-women",
+              },
+            ]}
+          />
+        </div>
+      </div>
+
+      {!filtered?.length && <p>No matching posts found.</p>}
+
+      {!!filtered?.length && (
+        <table>
+          <thead>
+            <tr>
+              <th>Title ( RefID )</th>
+              <th>Category</th>
+              <th>Slug</th>
+              <th>Locale</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {filtered?.map((page, index) => (
+              <tr key={index}>
+                <td>
+                  <strong>
+                    {index + 1}. {page.title} {page.refId && `(${page.refId})`}
+                  </strong>
+                </td>
+                <td>{page.category}</td>
+                <td>{page.slug}</td>
+                <td>{getLocaleFromSlug(page.slug)}</td>
+                <td>
+                  <div style={{ display: "flex" }}>
+                    <Button
+                      iconOnly
+                      color="transparent"
+                      onClick={() => {
+                        window.open(
+                          `/${getLocaleFromSlug(page.slug)}/gifts/${
+                            page.category
+                          }/${getCleanSlug(page.slug)}`,
+                          "_blank",
+                        );
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faEye} />
+                    </Button>
+
+                    <Button
+                      iconOnly
+                      color="transparent"
+                      onClick={() => {
+                        window.open(
+                          `${PROJECT_GITHUB_REPO}/edit/main/src/content/gifts/${page.slug}.md`,
+                          "_blank",
+                        );
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faEdit} />
+                    </Button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
--- src/features/AdminPanel/types/IdeaPage.ts
+++ src/features/AdminPanel/types/IdeaPage.ts
@@ -1,5 +1,8 @@
+import type { Category } from "@src/types/category.ts";
+
 export interface IdeaPage {
   refId: string;
   title: string;
   slug: string;
+  category: Category;
 }
