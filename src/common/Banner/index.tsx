295876296a66b654b5ba9412c4339d32998ef134
Alex Nepsha <nepsha1986@gmail.com>
2023-12-04 13:14:29 +0300

    fix. Banner styles, tinny fix (#76)

--- src/common/Banner/index.tsx
+++ src/common/Banner/index.tsx
@@ -8,9 +8,8 @@ interface Props {
 const Banner: React.FC<Props> = ({ image, text }) => {
   return (
     <div className={styles.banner}>
-      <div className={styles.banner__image}>{image}</div>
-
       <p className={styles.banner__text}>{text}</p>
+      <div className={styles.banner__image}>{image}</div>
     </div>
   );
 };
