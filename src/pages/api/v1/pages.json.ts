f2d3d50f19b1cf81038da04cea5418e33c359d38
Alex Nepsha <nepsha1986@gmail.com>
2023-12-06 22:27:20 +0300

    Add api draft(#77)

---
+++ src/pages/api/v1/pages.json.ts
@@ -0,0 +1,19 @@
+import { getCollection } from "astro:content";
+import {
+  type IdeaPage,
+  type ModuleName,
+} from "@src/features/AdminPanel/types/IdeaPage.ts";
+import type { Category } from "@src/types/category.ts";
+
+const giftsEntries = await getCollection("gifts");
+
+const pages: IdeaPage[] = giftsEntries.map((i) => ({
+  title: i.data.title,
+  slug: i.slug,
+  category: i.data.category as Category,
+  modules: (i.data.modules as ModuleName[]) || [],
+}));
+
+export async function GET(): Promise<Response> {
+  return new Response(JSON.stringify(pages));
+}
