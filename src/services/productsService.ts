b3247b5742a308a6321ef21b8c2b02422df48ff9
Alex Nepsha <nepsha1986@gmail.com>
2024-02-07 11:39:25 +0300

    reafctor (#103)

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -20,11 +20,8 @@ interface Params extends PaginatedParams, SearchableParams {
   locale?: string;
 }
 interface ProductsService {
-  add: (product: Omit<ProductDto, "_id">) => Promise<void>;
   get: (id: string) => Promise<ProductDto>;
   getAll: (params?: Params) => Promise<PageableRes<ProductDto>>;
-  update: (id: string, product: Omit<ProductDto, "_id">) => Promise<ProductDto>;
-  delete: (id: string) => Promise<void>;
 }
 
 const productsService: ProductsService = {
@@ -33,21 +30,12 @@ const productsService: ProductsService = {
 
     return data;
   },
-  update: async (id, data) => {
-    return await giftsAPI.put(`api/v1/products/${id}`, data);
-  },
   getAll: async (params) => {
     const { data } = await giftsAPI.get("api/v1/products", {
       params,
     });
 
     return data;
-  },
-  add: async (product) => {
-    await giftsAPI.post("api/v1/products", product);
-  },
-  delete: async (id) => {
-    await giftsAPI.delete(`api/v1/products/${id}`);
   },
 };
 
