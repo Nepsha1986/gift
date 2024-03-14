d4fdd1089738c793d01b0ec4754195b96ddab31f
Alex Nepsha <nepsha1986@gmail.com>
2024-03-14 12:52:59 +0300

    feature(Sitemap): Trying to fix site indexing issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://gift-idea.co",
-  integrations: [
-    react(),
-    mdx(),
-    sitemap({
-      filter: (page) => page !== "https://gift-idea.co/admin/",
-      i18n: {
-        defaultLocale: "en-us",
-        locales: {
-          "en-us": "en-US",
-          "ru-ua": "ru-UA",
-          "uk-ua": "uk-UA",
-        },
-      },
-    }),
-  ],
+  integrations: [react(), mdx(), sitemap()],
 });
