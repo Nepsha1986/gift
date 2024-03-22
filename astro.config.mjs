59953d57475cde3f93799122599097a888d6ee12
Alex Nepsha <nepsha1986@gmail.com>
2024-03-22 20:10:19 +0300

    feature(Posts pagination): Base implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://gift-idea.co",
+  redirects: {
+    "/[lang]/posts": {
+      status: 302,
+      destination: "/[lang]/posts/1",
+    },
+  },
   integrations: [react(), mdx(), sitemap()],
 });
