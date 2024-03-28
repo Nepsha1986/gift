caa768c81db26644b0d3029d93345df33d67c527
Alex Nepsha <nepsha1986@gmail.com>
2024-03-28 12:11:58 +0300

    hotfix(Single Post endpoint)

--- src/pages/api/v1/posts/[locale]/[slug].json.ts
+++ src/pages/api/v1/posts/[locale]/[slug].json.ts
@@ -4,6 +4,11 @@ import { getCleanSlug, getLocaleFromSlug } from "@i18n/utils.ts";
 
 const postsEntries = await getCollection("posts");
 
+const getImageName = (path: string): string => {
+  const parts = path.split("/");
+  return parts.slice(-1)[0];
+};
+
 export function getStaticPaths(): any {
   return postsEntries.map((i) => ({
     params: { locale: getLocaleFromSlug(i.slug), slug: getCleanSlug(i.slug) },
@@ -17,6 +22,10 @@ export function GET({ params }: APIContext): Response {
       getCleanSlug(i.slug) === params.slug,
   );
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const imageName = getImageName(filtered?.data.thumbnail.fsPath);
+
   const data = {
     id: filtered?.id,
     title: filtered?.data.title,
@@ -24,8 +33,9 @@ export function GET({ params }: APIContext): Response {
     locale: getLocaleFromSlug(filtered?.slug as string),
     author: filtered?.data.author,
     date: filtered?.data.date,
-    thumbnail: filtered?.data.thumbnail,
+    thumbnail: imageName,
     content: filtered?.body,
+    description: filtered?.data.description,
   };
 
   return new Response(JSON.stringify(data));
